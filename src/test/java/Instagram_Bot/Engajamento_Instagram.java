553d6ccaed384650d46804a1472bf381ec21d046
Jordan Oliveira <jordan.1oliveira@hotmail.com>
2021-12-03 11:11:05 -0300

    teste

--- src/test/java/Instagram_Bot/Engajamento_Instagram.java
+++ src/test/java/Instagram_Bot/Engajamento_Instagram.java
@@ -136,7 +136,7 @@ public class Engajamento_Instagram {
         suporte.wait(navegador, instagram.getNext());
         navegador.findElement(instagram.getNext()).click();
 
-        for (int i = 0; i <= 30; i++) {
+        for (int i = 0; i <= 35; i++) {
             Thread.sleep(4000);
             suporte.wait(navegador, instagram.getNext2());
             navegador.findElement(instagram.getNext2()).click();
